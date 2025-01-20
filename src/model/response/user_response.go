23a36b59d5449a5d5de1660ca206e4f2112202fe
Bruno Santos <bruno_st17@hotmail.com>
2025-01-19 23:54:42 -0300

    fix: testes

--- src/model/response/user_response.go
+++ src/model/response/user_response.go
@@ -3,6 +3,6 @@ package request
 type UserResponse struct {
 	ID    string `json:"id"`
 	Email string `json:"email"  binding:"required,min=4,max=50"`
-	Name  string `json:"name" binding:"required,min=4,max=50"`
+	Name  string `json:"name" binding:"required,min=4,max=30"`
 	Age   int8   `json:"age"`
 }
