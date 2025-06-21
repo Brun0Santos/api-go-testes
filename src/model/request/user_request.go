f6625a4dcd0a9709d38a3bfac738427106040885
Bruno Santos <bruno_st17@hotmail.com>
2025-06-20 23:22:01 -0300

    fix: testes

--- src/model/request/user_request.go
+++ src/model/request/user_request.go
@@ -3,6 +3,6 @@ package request
 type UserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=17,containsany=!@#$%"`
-	Name     string `json:"name" binding:"required,min=4,max=51"`
+	Name     string `json:"name" binding:"required,min=4,max=54"`
 	Age      int8   `json:"age" binding:"required,min=4,max=11"`
 }
